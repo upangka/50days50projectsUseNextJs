ffae5eeb2c264da3734e416f2a26a5f6441173ed
pkmer <q10viking@outlook.com>
2025-03-26 02:12:48 +0800

    feat(clock): 完成第一版的clock

--- src/app/(cases)/theme-clock/page.tsx
+++ src/app/(cases)/theme-clock/page.tsx
@@ -15,15 +15,10 @@ type ClockIndicator = {
 
 export default function ThemeClockPage() {
   const [secondDegree, setSecondDegree] = useState(0)
-  console.log({ secondDegree })
+  const [minuteDegree, setMinuteDegree] = useState(0)
+  const [hourDegree, setHourDegree] = useState(0)
   const scale = useCallback(
-    (
-      num: number,
-      in_min: number = 0,
-      in_max: number = 60,
-      out_min: number = 0,
-      out_max: number = 360
-    ) => {
+    (num: number, in_min: number, in_max: number, out_min: number = 0, out_max: number = 360) => {
       const degree = ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
       console.log({ degree })
       return degree
@@ -44,28 +39,72 @@ export default function ThemeClockPage() {
     const interval = setInterval(() => {
       const date = new Date()
       const seconds = date.getSeconds()
-      console.log(seconds)
-      setSecondDegree(scale(seconds))
+      const minute = date.getMinutes() + seconds / 60
+      const hour = (date.getHours() % 12 || 0) + minute / 60
+      setSecondDegree(scale(seconds, 0, 60))
+      setMinuteDegree(scale(minute, 0, 60))
+      setHourDegree(scale(hour, 0, 12))
     }, 1000)
     return () => {
       clearInterval(interval)
     }
   }, [])
 
+  // todo 基于长度计算
   return (
     <section className='flex h-screen w-screen justify-center bg-white pt-3.5'>
-      <div className='relative h-[420px] w-[420px]'>
+      <div className='relative h-[410px] w-[410px]'>
         {/* 圆圈start */}
-        <div className='absolute -top-[10px] -left-[6px] h-full w-full rounded-full border-8 border-amber-500'></div>
+        <div className='absolute -top-[5px] z-20 h-full w-full rounded-full border-8 border-green-600'>
+          {/* 时钟中心圆圈start */}
+          <div className='absolute top-1/2 left-1/2 z-20 h-[20px] w-[20px] -translate-1/2 rotate-180 rounded-full bg-amber-500'>
+            {/* 时针start */}
+            <div
+              style={{
+                zIndex: '50',
+                transformOrigin: 'top center',
+                transition: secondDegree != 0 ? 'transform 0.5s ease-in-out' : 'none',
+                transform: `rotate(${hourDegree}deg)`
+              }}
+              className='absolute left-1/2 h-[100px] w-[11px] translate-[10px] -translate-x-1/2 rounded-3xl bg-yellow-600 select-none'
+            ></div>
+            {/* 时针end */}
+
+            {/* 分针start */}
+            <div
+              style={{
+                zIndex: '50',
+                transformOrigin: 'top center',
+                transition: secondDegree != 0 ? 'transform 0.5s ease-in-out' : 'none',
+                transform: `rotate(${minuteDegree}deg)`
+              }}
+              className='absolute left-1/2 h-[130px] w-[8px] translate-[10px] -translate-x-1/2 rounded-3xl bg-blue-600 select-none'
+            ></div>
+            {/* 分针end */}
+
+            {/* 秒钟start */}
+            <div
+              style={{
+                zIndex: '50',
+                transformOrigin: 'top center',
+                transition: secondDegree != 0 ? 'transform 0.5s ease-in-out' : 'none',
+                transform: `rotate(${secondDegree}deg)`
+              }}
+              className='absolute left-1/2 h-[160px] w-[5px] translate-[10px] -translate-x-1/2 rounded-3xl bg-green-600 select-none'
+            ></div>
+            {/* 秒钟end */}
+          </div>
+          {/* 时钟中心圆圈end */}
+        </div>
         {/* 圆圈end */}
 
         {/* 数字start */}
-        <ul className='relative h-[400px] w-[400px] rounded-full bg-white'>
+        <ul className='relative z-10 h-[400px] w-[400px] rounded-full bg-white'>
           {clockIndicators.map(indicator => {
             return (
               <li
                 key={indicator.index}
-                className='absolute h-[200px] w-[15px] bg-white text-lg text-black select-none'
+                className='absolute h-[200px] w-[15px] bg-transparent text-lg text-black select-none'
                 style={{
                   left: '50%',
                   transform: `rotate(${indicator.degree}deg)`,
@@ -85,15 +124,6 @@ export default function ThemeClockPage() {
         </ul>
         {/* 数字end */}
       </div>
-
-      {/* <div
-        style={{
-          transformOrigin: 'bottom center',
-          transition: 'transform 0.5s ease-in-out',
-          transform: `rotate(${secondDegree}deg)`
-        }}
-        className='h-[200px] w-[5px] bg-black select-none'
-      ></div> */}
     </section>
   )
 }
