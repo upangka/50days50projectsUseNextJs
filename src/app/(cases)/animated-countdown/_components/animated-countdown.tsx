3961dc243bf720ea57e5ab5b1e04486339c4270a
pkmer <q10viking@outlook.com>
2025-04-03 23:56:41 +0800

    feat(animated countdown): finished animate countdown

--- src/app/(cases)/animated-countdown/_components/animated-countdown.tsx
+++ src/app/(cases)/animated-countdown/_components/animated-countdown.tsx
@@ -3,6 +3,7 @@ import clsx from 'clsx'
 import { useState } from 'react'
 import Styles from './animate-countdow.module.scss'
 import { originColors } from '@/utils/word'
+import { Button } from '@/components/pkmer-button'
 interface AnimatedCountdownProps {
   count: number
   colors?: string[]
@@ -11,6 +12,8 @@ interface AnimatedCountdownProps {
 const AnimatedCountdown: React.FC<AnimatedCountdownProps> = props => {
   const [currentIndex, setCurrentIndex] = useState(props.count)
   const [currentAnimation, setCurrentAnimation] = useState<'goIn' | 'goOut' | null>('goIn')
+  // 设置准备状态，确保最后一个0 GoIn结束之后再出现
+  const goOrReady = currentIndex === 0 && currentAnimation === null
 
   function handleAnimationEnd(e: React.AnimationEvent<HTMLLIElement>) {
     const animationName = e.animationName
@@ -23,6 +26,8 @@ const AnimatedCountdown: React.FC<AnimatedCountdownProps> = props => {
       if (currentIndex > 0) {
         // 当前数字添加goOut动画
         setCurrentAnimation('goOut')
+      } else {
+        setCurrentAnimation(null)
       }
     } else if (currentAnimation === 'goOut') {
       /**
@@ -35,27 +40,49 @@ const AnimatedCountdown: React.FC<AnimatedCountdownProps> = props => {
     }
   }
 
+  function handleBtnClick() {
+    // 重新开始
+    setCurrentIndex(props.count)
+    setCurrentAnimation('goIn')
+  }
+
   const count = props.count + 1
   return (
-    <ul className='relative h-[100px] w-[250px] overflow-hidden border-b-2 border-dashed border-white'>
-      {[...Array(count)].map((_, index) => (
-        <li
-          key={index}
-          style={{
-            color: originColors[index % originColors.length]
-          }}
-          onAnimationEnd={handleAnimationEnd}
-          className={clsx(
-            Styles.Number,
-            currentIndex === index && currentAnimation === 'goIn' && Styles.GoIn,
-            currentIndex === index && currentAnimation === 'goOut' && Styles.GoOut,
-            'absolute bottom-0 left-1/2 -translate-x-1/2 text-8xl'
-          )}
-        >
-          {index}
-        </li>
-      ))}
-    </ul>
+    <section className='w-[250px]'>
+      {goOrReady ? (
+        <>
+          <h1 className={clsx(Styles.GoTitle, 'mb-4 w-full text-center text-5xl font-bold')}>
+            Go Go Go
+          </h1>
+          <Button onClick={handleBtnClick} className='w-full'>
+            重新开始
+          </Button>
+        </>
+      ) : (
+        <>
+          <ul className='relative h-[100px] w-full overflow-hidden border-b-2 border-dashed border-white'>
+            {[...Array(count)].map((_, index) => (
+              <li
+                key={index}
+                style={{
+                  color: originColors[index % originColors.length]
+                }}
+                onAnimationEnd={handleAnimationEnd}
+                className={clsx(
+                  Styles.Number,
+                  currentIndex === index && currentAnimation === 'goIn' && Styles.GoIn,
+                  currentIndex === index && currentAnimation === 'goOut' && Styles.GoOut,
+                  'absolute bottom-0 left-1/2 -translate-x-1/2 text-8xl'
+                )}
+              >
+                {index}
+              </li>
+            ))}
+          </ul>
+          <h1 className='mt-4 w-full text-center text-3xl font-bold'>倒计时</h1>
+        </>
+      )}
+    </section>
   )
 }
 
