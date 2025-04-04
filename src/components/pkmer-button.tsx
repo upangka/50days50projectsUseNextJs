0823c11155a0c949816b033b264a358ba68c5528
pkmer <q10viking@outlook.com>
2025-04-05 00:32:10 +0800

    fix(Button): 修复不可点击状态

--- src/components/pkmer-button.tsx
+++ src/components/pkmer-button.tsx
@@ -21,7 +21,7 @@ export const Button: React.FC<ButtonProps> = ({
           'rounded-md px-3 py-2 font-bold text-white shadow-2xs transition-all duration-200 select-none',
           isEnable
             ? 'border border-green-400 bg-green-600 shadow-green-300 hover:bg-green-700 hover:shadow-green-400'
-            : 'border border-gray-400 bg-gray-600 shadow-gray-300'
+            : 'cursor-not-allowed border border-gray-400 bg-gray-600 shadow-gray-300'
         )}
         onClick={() => onClick()}
       >
