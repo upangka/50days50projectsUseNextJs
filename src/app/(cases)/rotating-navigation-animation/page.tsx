c4857df4e58480043d0bb975331f7360fa5a8aa0
pkmer <q10viking@outlook.com>
2025-04-06 11:47:35 +0800

    feat(circle): 优化circle转圈的颜色变化

--- src/app/(cases)/rotating-navigation-animation/page.tsx
+++ src/app/(cases)/rotating-navigation-animation/page.tsx
@@ -32,15 +32,15 @@ export default function RotatingNavigationAnimationPage() {
         {/* circle menu部分start */}
         <div
           className={clsx(
-            isMenuOpen && '-rotate-90',
-            'relative h-[200px] w-[200px] rounded-full bg-red-500 transition-all duration-500'
+            isMenuOpen ? '-rotate-90 bg-red-500' : 'bg-green-500',
+            'relative h-[200px] w-[200px] rounded-full transition-all duration-500'
           )}
         >
           <button
             onClick={() => setIsMenuOpen(true)}
             className='absolute right-1/4 bottom-1/4 translate-x-1/3 translate-y-1/3 cursor-pointer'
           >
-            <Icon icon='bytesize:menu' {...size} className='text-white hover:text-green-500' />
+            <Icon icon='bytesize:menu' {...size} className='text-white hover:text-red-500' />
           </button>
 
           <button
