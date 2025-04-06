8136cf01caf227bef9a54c9e70c73adde5817567
pkmer <q10viking@outlook.com>
2025-04-06 12:56:48 +0800

    fix(层级): 修复流动边框的层级问题

--- src/components/card/border-animated-card.tsx
+++ src/components/card/border-animated-card.tsx
@@ -29,8 +29,11 @@ const BorderAnimatedCard: React.FC<BorderAnimatedCardProps> = ({
 }) => {
   return (
     <div className={clsx(Styles.CardContainer, 'relative w-auto overflow-hidden rounded-md')}>
-      <main className={Styles.ContentContainer}>{children}</main>
-      {/* 显示边框颜色start */}
+      {/* 内容区域start 要求层级最高 */}
+      <main className={clsx(Styles.ContentContainer, 'relative z-30')}>{children}</main>
+      {/* 内容区域end */}
+
+      {/* 显示边框颜色start  层级最低*/}
       <div
         style={
           {
@@ -38,13 +41,13 @@ const BorderAnimatedCard: React.FC<BorderAnimatedCardProps> = ({
             '--animation-duration': `${duration}s`
           } as React.CSSProperties
         }
-        className={clsx(Styles.LineBg, 'absolute top-1/2 left-1/2 -z-20 h-[300%] w-[300%]')}
+        className={clsx(Styles.LineBg, 'absolute top-1/2 left-1/2 z-10 h-[300%] w-[300%]')}
       ></div>
       {/* 显示边框颜色end */}
-      {/* 空隙start */}
+      {/* 空隙start 层级中 */}
       <div
         style={{ backgroundColor: backgroundColor }}
-        className='absolute inset-1 -z-10 rounded-md'
+        className='absolute inset-1 z-20 rounded-md'
       ></div>
       {/* 空隙end */}
     </div>
