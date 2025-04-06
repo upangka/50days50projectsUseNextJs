560776031693654fc96fa404a36dfb800c23faa3
pkmer <q10viking@outlook.com>
2025-04-06 12:29:48 +0800

    refactor(抽离组件): 分离组件出来

--- src/components/nav/animation/rotating-navigation-animation.tsx
+++ src/components/nav/animation/rotating-navigation-animation.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from 'react'
+import { useState, memo } from 'react'
 import { clsx } from 'clsx'
 import type { NavItem } from './types'
 import { Icon } from '@iconify/react'
@@ -9,50 +9,105 @@ interface Props extends React.PropsWithChildren {
    * @default 36
    */
   menuIconSize?: number
+  /**
+   * 菜单列表
+   */
   navList: NavItem[]
 }
 
-const RotatingNavigationAnimation: React.FC<Props> = ({ menuIconSize = 36, navList, children }) => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
+const RotatingNavigationAnimation: React.FC<Props> = memo(
+  ({ menuIconSize = 36, navList, children }) => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+    return (
+      <section className='relative overflow-hidden bg-black'>
+        {/* memu start */}
+        <RotatingBtnCircle
+          menuIconSize={menuIconSize}
+          isMenuOpen={isMenuOpen}
+          onClose={() => setIsMenuOpen(false)}
+          onOpen={() => setIsMenuOpen(true)}
+        />
+        {/* memu end */}
+
+        {/* 菜单start */}
+        <RotatingNav navList={navList} isMenuOpen={isMenuOpen} />
+        {/* 菜单end */}
+
+        {/* content start */}
+        <main
+          style={{
+            transformOrigin: 'top left'
+          }}
+          className={clsx(
+            isMenuOpen &&
+              '-rotate-12 rounded-lg border-4 border-green-500/50 shadow-lg shadow-green-500/50',
+            'relative z-10 w-screen overflow-auto bg-white transition-all duration-500'
+          )}
+        >
+          {children}
+        </main>
+        {/* content end */}
+
+        {/* footer description start */}
+        <RotatingFooter isMenuOpen={isMenuOpen} />
+        {/* footer description end */}
+      </section>
+    )
+  }
+)
+
+/**
+ * 选装页面按钮控制面板
+ */
+const RotatingBtnCircle: React.FC<{
+  menuIconSize: number
+  isMenuOpen: boolean
+  onClose: () => void
+  onOpen: () => void
+}> = memo(({ onClose, onOpen, isMenuOpen, menuIconSize }) => {
   const size = {
     width: menuIconSize,
     height: menuIconSize
   }
-
   return (
-    <section className='relative overflow-hidden bg-black'>
-      {/* memu start */}
-      <div className='fixed top-0 left-0 z-50 -translate-1/2'>
-        {/* circle menu部分start */}
-        <div
-          className={clsx(
-            isMenuOpen ? '-rotate-90 bg-red-500' : 'bg-green-500',
-            'relative h-[200px] w-[200px] rounded-full transition-all duration-500'
-          )}
+    <div className='fixed top-0 left-0 z-50 -translate-1/2'>
+      {/* circle menu部分start */}
+      <div
+        className={clsx(
+          isMenuOpen ? '-rotate-90 bg-red-500' : 'bg-green-500',
+          'relative h-[200px] w-[200px] rounded-full transition-all duration-500'
+        )}
+      >
+        <button
+          onClick={onOpen}
+          className='absolute right-1/4 bottom-1/4 translate-x-1/3 translate-y-1/3 cursor-pointer'
         >
-          <button
-            onClick={() => setIsMenuOpen(true)}
-            className='absolute right-1/4 bottom-1/4 translate-x-1/3 translate-y-1/3 cursor-pointer'
-          >
-            <Icon icon='bytesize:menu' {...size} className='text-white hover:text-red-500' />
-          </button>
+          <Icon icon='bytesize:menu' {...size} className='text-white hover:text-red-500' />
+        </button>
 
-          <button
-            onClick={() => setIsMenuOpen(false)}
-            className='absolute bottom-1/4 left-1/4 -translate-x-1/3 translate-y-1/3 cursor-pointer'
-          >
-            <Icon
-              icon='flowbite:close-outline'
-              {...size}
-              className='text-white hover:text-green-500'
-            />
-          </button>
-        </div>
-        {/* circle menu部分end */}
+        <button
+          onClick={onClose}
+          className='absolute bottom-1/4 left-1/4 -translate-x-1/3 translate-y-1/3 cursor-pointer'
+        >
+          <Icon
+            icon='flowbite:close-outline'
+            {...size}
+            className='text-white hover:text-green-500'
+          />
+        </button>
       </div>
-      {/* memu end */}
+      {/* circle menu部分end */}
+    </div>
+  )
+})
 
-      {/* 菜单start */}
+/**
+ * 旋转显示的菜单列表
+ */
+const RotatingNav: React.FC<{ navList: NavItem[]; isMenuOpen: boolean }> = memo(
+  ({ navList, isMenuOpen }) => {
+    return (
       <nav className='fixed bottom-0 left-2 -translate-y-3/4'>
         <ul className='flex flex-col items-center gap-4'>
           {navList.map((item, index) => (
@@ -81,55 +136,46 @@ const RotatingNavigationAnimation: React.FC<Props> = ({ menuIconSize = 36, navLi
           ))}
         </ul>
       </nav>
-      {/* 菜单end */}
+    )
+  }
+)
 
-      {/* content start */}
-      <main
+/**
+ * 旋转之后的footer展示
+ */
+const RotatingFooter: React.FC<{ isMenuOpen: boolean }> = memo(({ isMenuOpen }) => {
+  return (
+    <footer className='absolute right-1/12 bottom-1/12 flex flex-col gap-2'>
+      <p
         style={{
-          transformOrigin: 'top left'
+          transform: isMenuOpen ? `translateY(0)` : 'translateY(200%)',
+          transition: 'transform 0.5s ease-in-out',
+          transitionDelay: isMenuOpen ? `${1 * 0.1}s` : '0s'
         }}
-        className={clsx(
-          isMenuOpen &&
-            '-rotate-12 rounded-lg border-4 border-green-500/50 shadow-lg shadow-green-500/50',
-          'relative z-10 w-screen overflow-auto bg-white transition-all duration-500'
-        )}
+        className='text-left text-xl'
       >
-        {children}
-      </main>
-      {/* content end */}
-
-      {/* footer description start */}
-      <footer className='absolute right-1/12 bottom-1/12 flex flex-col gap-2'>
-        <p
-          style={{
-            transform: isMenuOpen ? `translateY(0)` : 'translateY(200%)',
-            transition: 'transform 0.5s ease-in-out',
-            transitionDelay: isMenuOpen ? `${1 * 0.1}s` : '0s'
-          }}
-          className='text-left text-xl'
-        >
-          I Love <span className='text-green-500'>React</span> And{' '}
-          <span className='text-green-500'>Next.js</span>
-        </p>
-        <p
-          style={{
-            transform: isMenuOpen ? `translateY(0)` : 'translateY(200%)',
-            transition: 'transform 0.5s ease-in-out',
-            transitionDelay: isMenuOpen ? `${2 * 0.1}s` : '0s'
-          }}
-          className='text-left text-xl'
-        >
-          Happy Coding <span className='text-2xl text-yellow-500'>:)</span>🥳 from{' '}
-          <a href='https://gitee.com/pkmer/50days50projects-use-next-js' className='text-green-500'>
-            Pkmer
-          </a>
-        </p>
-      </footer>
-      {/* footer description end */}
-    </section>
+        I Love <span className='text-green-500'>React</span> And{' '}
+        <span className='text-green-500'>Next.js</span>
+      </p>
+      <p
+        style={{
+          transform: isMenuOpen ? `translateY(0)` : 'translateY(200%)',
+          transition: 'transform 0.5s ease-in-out',
+          transitionDelay: isMenuOpen ? `${2 * 0.1}s` : '0s'
+        }}
+        className='text-left text-xl'
+      >
+        Happy Coding <span className='text-2xl text-yellow-500'>:)</span>🥳 from{' '}
+        <a href='https://gitee.com/pkmer/50days50projects-use-next-js' className='text-green-500'>
+          Pkmer
+        </a>
+      </p>
+    </footer>
   )
-}
+})
 
 RotatingNavigationAnimation.displayName = 'RotatingNavigationAnimation'
-
+RotatingBtnCircle.displayName = 'RotatingBtnCircle'
+RotatingNav.displayName = 'RotatingNav'
+RotatingFooter.displayName = 'RotatingFooter'
 export default RotatingNavigationAnimation
