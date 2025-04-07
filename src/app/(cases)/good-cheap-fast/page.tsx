9587b4ae1b46e9fd3d475a683d1f2428394ee4d4
pkmer <q10viking@outlook.com>
2025-04-07 20:51:34 +0800

    feat(good cheap fast): 抽离操作面板

--- src/app/(cases)/good-cheap-fast/page.tsx
+++ src/app/(cases)/good-cheap-fast/page.tsx
@@ -122,33 +122,60 @@ export default function GoodCheapFastPage() {
       {/* msg start */}
       {isShowMsgPrompt && <ProjectMsgPrompt msg={msg} />}
       {/* msg end */}
-      <div className='relative -translate-x-1/2 rounded-lg bg-white p-10 text-black shadow-lg shadow-white'>
-        <h1 className='mb-3 text-xl font-bold'>
-          甲方の终极难题：
-          <br /> 快、好、省，您想放弃哪一个？
-        </h1>
-        <ul className='flex flex-col items-start justify-start gap-2.5'>
-          {projectPriorities.map((project, index) => (
-            <li key={index} className='flex items-center gap-2'>
-              <ToggleBall
-                isOpen={chooseStates[index]}
-                onChange={isOpen => {
-                  handleChange(isOpen, index)
-                }}
-              />
-              <span className='text-xl'>{project}</span>
-            </li>
-          ))}
-        </ul>
 
-        {/* 操作日志start */}
-        {history.current.length >= 0 && <HistoryTerminal ref={ulRef} history={history.current} />}
-        {/* 操作日志end */}
-      </div>
+      {/* 操作面板start */}
+      <ProjectOperationBoard
+        chooseStates={chooseStates}
+        history={history.current}
+        ref={ulRef}
+        onToggleBallChange={handleChange}
+      />
+      {/* 操作面板end */}
     </section>
   )
 }
 
+// ============ProjectOperationBoard=======================
+interface ProjectOperationBoardProps {
+  chooseStates: boolean[]
+  history: string[]
+  ref?: Ref<HTMLUListElement>
+  onToggleBallChange: (isOpen: boolean, index: number) => void
+}
+const ProjectOperationBoard: React.FC<ProjectOperationBoardProps> = ({
+  chooseStates,
+  history,
+  ref,
+  onToggleBallChange
+}) => {
+  return (
+    <div className='relative -translate-x-1/2 rounded-lg bg-white p-10 text-black shadow-lg shadow-white'>
+      <h1 className='mb-3 text-xl font-bold'>
+        甲方の终极难题：
+        <br /> 快、好、省，您想放弃哪一个？
+      </h1>
+      <ul className='flex flex-col items-start justify-start gap-2.5'>
+        {projectPriorities.map((project, index) => (
+          <li key={index} className='flex items-center gap-2'>
+            <ToggleBall
+              isOpen={chooseStates[index]}
+              onChange={isOpen => {
+                onToggleBallChange(isOpen, index)
+              }}
+            />
+            <span className='text-xl'>{project}</span>
+          </li>
+        ))}
+      </ul>
+
+      {/* 操作日志start */}
+      {history.length >= 0 && <HistoryTerminal ref={ref} history={history} />}
+      {/* 操作日志end */}
+    </div>
+  )
+}
+
+ProjectOperationBoard.displayName = 'ProjectOperationBoard'
 // ============Project Msg Prompt=============================
 
 interface ProjectMsgPromptProps {
